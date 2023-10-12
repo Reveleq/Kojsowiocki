dd4aa01c1ce0588cd4799752c2343e6e711f465e
MacbookReveleq <damianw182@interia.pl>
2023-10-12 19:17:13 +0200

    added about-us and news sections

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ const burgerBtn = document.querySelector(".mobile-nav__btn");
 const closeBtn = document.querySelector(".mobile-nav__close-btn");
 const mobileNavLinks = document.querySelector(".mobile-nav__links");
 const mobileNavBg = document.querySelector('.mobile-nav__bg')
+const mobileNavLink = document.querySelectorAll('.mobile-nav__link')
 const showLinks = () => {
 	mobileNavLinks.classList.add("active");
 	closeBtn.classList.add("active-btn");
 	burgerBtn.classList.remove("active-btn");
 	burgerBtn.classList.add("hide");
+    mobileNavBg.classList.add('active-btn')
     setTimeout( () => {
         mobileNavLinks.classList.add("slide")}, 1)
     mobileNavBg.classList.add('opacity')
@@ -24,7 +26,10 @@ const hideLinks = () => {
      setTimeout( () => {
         mobileNavLinks.classList.remove("active")
         closeBtn.classList.remove("active-btn");
+        mobileNavBg.classList.add('active-btn')
 }, 300)
 };
+mobileNavLink.forEach( e => e.addEventListener('click', hideLinks)
+)
 burgerBtn.addEventListener("click", showLinks);
 closeBtn.addEventListener("click", hideLinks);
